0eb4291224fe15bd4d866ab27b105aa9122f088c
karpov-dev <passangerfeat@gmail.com>
2020-09-23 10:02:51 +0300

    - Code changes

--- main.py
+++ main.py
@@ -3,37 +3,10 @@ import re
 import argparse
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--file_dir', help='Path to JSON file')
-parser.add_argument('--file_name', help='JSON file name')
-parser.add_argument('--update_dict', help='word_to_change:new_word,word_to_change:new_word')
-parser.add_argument('--auto_update', help='True or False. Auto change words or manual changing. Default - manual')
-args = parser.parse_args()
-
-
-if not args.file_dir:
-    raise SystemExit('ERROR. Enter file dir.')
-FILE_DIR = args.file_dir
-
-if not args.file_name:
-    raise SystemExit('ERROR. Enter file name.')
-FILE_NAME = args.file_name
-
-if not args.update_dict:
-    raise SystemExit('ERROR. Enter update dict.')
-
-UPDATE_DICT = {}
-for update_dict_arg in args.update_dict.replace('_', ' ').split(','):
-    key_value = update_dict_arg.split(':')
-    UPDATE_DICT[key_value[0]] = key_value[1]
-
-AUTO_UPDATE = False
-
-
 def find_usages(json_data, path):
     if type(json_data) is dict:
         for key in json_data:
-            find_usages(json_data[key], key)
+            json_data[key] = find_usages(json_data[key], key)
     elif type(json_data) is list:
         for list_element in json_data:
             find_usages(list_element, 'list_element')
@@ -48,8 +21,48 @@ def find_usages(json_data, path):
                         json_data = json_data.replace(element_to_update, UPDATE_DICT[element_to_update])
                         print('New value: ' + json_data)
                 print('_________________________________________________________________________________')
+    return json_data
 
+
+DEBUG_MODE = False
+if DEBUG_MODE:
+    UPDATE_DICT = {'Kate Client': 'THE BACKGROUND', 'test': 'TEST'}
+    AUTO_UPDATE = True
+    FILE_DIR = '/home/user/Downloads'
+    FILE_NAME = 'dataFile.json'
+else:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file_dir', help='Path to JSON file')
+    parser.add_argument('--file_name', help='JSON file name')
+    parser.add_argument('--update_dict', help='word_to_change:new_word,word_to_change:new_word and etc.')
+    parser.add_argument('--auto_update', help='True or False. Enable Auto change words or manual changing. '
+                                              'Default - manual')
+    args = parser.parse_args()
+
+    if not args.file_dir:
+        raise SystemExit('ERROR. Enter file dir. For instance: /home/user/Downloads')
+    FILE_DIR = args.file_dir
+
+    if not args.file_name:
+        raise SystemExit('ERROR. Enter file name. For instance: dataFile.json')
+    FILE_NAME = args.file_name
+
+    if not args.update_dict:
+        raise SystemExit('ERROR. Enter update dict. For instance: new_word,word_to_change:new_word')
+
+    UPDATE_DICT = {}
+    for update_dict_arg in args.update_dict.replace('_', ' ').split(','):
+        key_value = update_dict_arg.split(':')
+        UPDATE_DICT[key_value[0]] = key_value[1]
+
+    AUTO_UPDATE = False
+    if args.auto_update:
+        AUTO_UPDATE = bool(args.auto_update)
 
 with open(FILE_DIR + '/' + FILE_NAME) as file:
     json_data = json.load(file)
 find_usages(json_data, 'main')
+
+with open(FILE_DIR + '/' + 'updated_' + FILE_NAME, 'w') as file:
+    json.dump(json_data, file)
+print('Done!')
