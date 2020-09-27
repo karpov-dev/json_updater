28c00937efce6c1da917ce736ad85f8600383141
karpov-dev <passangerfeat@gmail.com>
2020-09-27 17:05:39 +0300

    - Code changes

--- main.py
+++ main.py
@@ -3,41 +3,115 @@ import re
 import argparse
 
 
-def find_usages(json_data, path):
-    if type(json_data) is dict:
-        for key in json_data:
-            json_data[key] = find_usages(json_data[key], key)
-    elif type(json_data) is list:
-        for list_element in json_data:
-            find_usages(list_element, 'list_element')
-    elif type(json_data) is str:
-        for element_to_update in UPDATE_DICT:
-            if re.findall(r'\b' + element_to_update + r'\b', json_data, re.IGNORECASE):
-                print('Word: ' + element_to_update + '\nPath: ' + path + '\nValue: ' + json_data)
-                if AUTO_UPDATE:
-                        json_data = json_data.replace(element_to_update, UPDATE_DICT[element_to_update])
-                else:
-                    if input('Change to: ' + UPDATE_DICT[element_to_update] + '?\ny/n\nAnswer: ') == 'y':
-                        json_data = json_data.replace(element_to_update, UPDATE_DICT[element_to_update])
-                        print('New value: ' + json_data)
-                print('_________________________________________________________________________________')
-    return json_data
+YEAR = 'year'
+MONTH = 'month'
+DAY = 'day'
+UPDATE = 'update'
+DATE = 'date'
+WORDS_DICT = {'Kate Client': 'THE BACKGROUND'}
+FILE_DIR = '/home/user/Downloads'
+FILE_NAME = 'dataFile.json'
+MODE = UPDATE
+NEW_DATE_VALUE = '01'
+AUTO_MODE = False
+CONSOLE_MODE = False
 
 
-DEBUG_MODE = False
-if DEBUG_MODE:
-    UPDATE_DICT = {'Kate Client': 'THE BACKGROUND', 'test': 'TEST'}
-    AUTO_UPDATE = True
-    FILE_DIR = '/home/user/Downloads'
-    FILE_NAME = 'dataFile.json'
-else:
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--file_dir', help='Path to JSON file')
-    parser.add_argument('--file_name', help='JSON file name')
-    parser.add_argument('--update_dict', help='word_to_change:new_word,word_to_change:new_word and etc.')
-    parser.add_argument('--auto_update', help='True or False. Enable Auto change words or manual changing. '
-                                              'Default - manual')
-    args = parser.parse_args()
+def find_and_update(data_string, data_path, regex_pattern, substring, auto_mode):
+    # Find substring in data_string and update change it
+    if not data_string:
+        raise ValueError('Empty string to find substring')
+    if not regex_pattern:
+        raise ValueError('Empty regex pattern to find substring')
+    if not substring:
+        raise ValueError('Empty new substring to replace')
+
+    if re.findall(regex_pattern, data_string, re.IGNORECASE):
+        print('Path: ' + data_path + '\nChange in: ' + data_string + '\nFrom: '
+              + re.search(regex_pattern, data_string).string + '\nTo: ' + substring)
+        if not auto_mode and input('y/n\nAnswer: ') == 'n':
+            print('CONTINUE\n')
+            return data_string
+        data_string = re.sub(regex_pattern, substring, data_string)
+        print('New string: ' + data_string + '\n')
+    return data_string
+
+
+def change_date(data_string, date_mode, new_value, auto_mode):
+    # find date and update part of date
+    if not data_string:
+        return data_string
+    if not date_mode:
+        raise ValueError('Empty date mode')
+    if not new_value:
+        raise ValueError('Empty new date value')
+
+    regex_pattern = r'\b(3[01]|[12][0-9]|0?[1-9])/(1[012]|0?[1-9])/((?:19|20)\d{2})\b'
+    if re.findall(regex_pattern, data_string, re.IGNORECASE):
+        old_value = data_string
+        date_elements = re.split(r'/', data_string)
+        if date_mode == DAY:
+            date_elements[0] = new_value
+        elif date_mode == MONTH:
+            date_elements[1] = new_value
+        elif date_mode == YEAR:
+            date_elements[2] = new_value
+        new_date = '/'.join(date_elements)
+        if not auto_mode and input('Change from ' + old_value + ' to ' + new_date + '?\nAnswer[y/n]: ') == 'y':
+            print('Changed to: ' + new_date + '\n')
+            return new_date
+        print('CONTINUE' + '\n')
+        return old_value
+
+
+def find_and_update_from_dict(json_string, json_path, convert_dict, auto_mode):
+    if not json_string:
+        return json_string
+
+    for key in convert_dict:
+        regex_pattern = r'\b' + key + r'\b'
+        json_string = find_and_update(json_string, json_path, regex_pattern, convert_dict[key], auto_mode)
+    return json_string
+
+
+def read_json_file(file_dir, file_name):
+    with open(file_dir + '/' + file_name) as json_file:
+        data = json.load(json_file)
+    return data
+
+
+def write_json_file(file_dir, file_name, data):
+    with open(file_dir + '/' + 'updated_' + file_name, 'w') as json_file:
+        json.dump(data, json_file)
+
+
+def processing_json_data(json_element, path):
+    if type(json_element) is dict:
+        for key in json_element:
+            json_element[key] = processing_json_data(json_element[key], key)
+    elif type(json_element) is list:
+        for list_element in json_element:
+            processing_json_data(list_element, 'list_element')
+    elif type(json_element) is str:
+        if MODE == 'update':
+            return find_and_update_from_dict(json_element, path, WORDS_DICT, AUTO_MODE)
+        if MODE == 'date':
+            return change_date(json_element, DAY, NEW_DATE_VALUE, AUTO_MODE)
+    return json_element
+
+
+def parsing_console_args():
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument('--file_dir', help="Path to dir with Json. Need use without '/'. For example: /home/data")
+    args_parser.add_argument('--file_name', help="Json file name. For example: data_file.json")
+    args_parser.add_argument('--auto_mode', help="Enable program auto mode.")
+    args_parser.add_argument('--mode', help="Select program mode. May be: update, date.")
+    args_parser.add_argument('--date_part', help="Select date part of date mode. May be: day, month, year.")
+    args_parser.add_argument('--new_date_part', help="Set new date part to change date. For example: 01, 27, 2021.")
+    args_parser.add_argument('--update_dict', help="Words to change. "
+                                                   "Format: word_to_change:new_word,word_to_change:new_word. '_' will"
+                                                   "replacing to ' ' (space).")
+    args = args_parser.parse_args()
 
     if not args.file_dir:
         raise SystemExit('ERROR. Enter file dir. For instance: /home/user/Downloads')
@@ -47,22 +121,26 @@ else:
         raise SystemExit('ERROR. Enter file name. For instance: dataFile.json')
     FILE_NAME = args.file_name
 
-    if not args.update_dict:
-        raise SystemExit('ERROR. Enter update dict. For instance: new_word,word_to_change:new_word')
+    if args.mode == UPDATE:
+        if not args.update_dict:
+            raise SystemExit('ERROR. Enter update dict. For instance: new_word,word_to_change:new_word')
+    elif args.mode == DATE:
+        if not args.date_part:
+            raise SystemExit('ERROR. Enter date part. May be: day, month, year.')
+        if not args.new_date_part:
+            raise SystemExit('ERROR. Enter new date part value')
 
-    UPDATE_DICT = {}
     for update_dict_arg in args.update_dict.replace('_', ' ').split(','):
         key_value = update_dict_arg.split(':')
-        UPDATE_DICT[key_value[0]] = key_value[1]
+        WORDS_DICT[key_value[0]] = key_value[1]
 
-    AUTO_UPDATE = False
     if args.auto_update:
         AUTO_UPDATE = bool(args.auto_update)
 
-with open(FILE_DIR + '/' + FILE_NAME) as file:
-    json_data = json.load(file)
-find_usages(json_data, 'main')
 
-with open(FILE_DIR + '/' + 'updated_' + FILE_NAME, 'w') as file:
-    json.dump(json_data, file)
+if CONSOLE_MODE:
+    parsing_console_args()
+json_data = read_json_file(FILE_DIR, FILE_NAME)
+processing_json_data(json_data, 'json_file')
+write_json_file(FILE_DIR, FILE_NAME, json_data)
 print('Done!')
